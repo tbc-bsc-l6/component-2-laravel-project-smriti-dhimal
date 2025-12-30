7fca69acce3a975e106e3d2063925f30d9d95b61
smritidhimal <dsmriti23@tbc.edu.np>
2025-12-30 13:26:51 +0545

    Add header with site name and nav links

---
+++ educationalSite/resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Header</title>
+</head>
+<body>
+<header class="header">
+    <div class="header-container">
+        <div class="site-name">educationalSite</div>
+        <nav class="nav-links">
+            <a href="/homepage">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/modules">Modules</a>
+            <a href="/login" class="login-button">Login</a>
+            <a href="/register" class="register-button">Register</a>
+</nav>
+</div>
+</header>
+</body>
+</html>
+
+
+
