aea4ccb039c0058854f74e8272b4b82ed06a4414
smritidhimal <dsmriti23@tbc.edu.np>
2026-01-11 00:33:02 +0545

    Create core models

--- educationalSite/app/Models/User.php
+++ educationalSite/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,8 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_role_id',
     ];
 
+    protected $guarded = ['id', 'remembre_token', 'email_verified_at'];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -44,5 +45,49 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(UserRole::class, 'user_role_id');
+    }
+
+    public function modules()
+    {
+        return $this->belongsToMany(Module::class)
+        ->withPivot('start_date', 'completed_at', 'result')
+        ->withTimestamps();
+    }
+
+    public function activeModules()
+    {
+        return $this->modules()->wherePivotNull('completed_at');
+    }
+
+    public function completedModules()
+    {
+        return $this->modules()->wherePivotNotNull('completed_at');
+    }
+
+    public function assignedModules()
+    {
+        return $this->hasMany(Module::class, 'teacher_id');
+    }
+    // Helper methods for role checking
+    public function isAdmin()
+    {
+        return $this->user_role_id === 1;
+    }
+    public function isTeacher()
+    {
+        return $this->user_role_id === 2;
+    }
+    public function isCurrentStudent()
+    {
+        return $this->user_role_id === 3;
+    }
+    public function isOldStudent()
+    {
+        return $this->user_role_id === 4;
     }
 }
