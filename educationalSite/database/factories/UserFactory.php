25bb1f87b82c6bbbb4658cb7ac9cdc165d00b874
smritidhimal <dsmriti23@tbc.edu.np>
2026-01-11 00:56:06 +0545

    Update UserFactory to support roles and helper states

--- educationalSite/database/factories/UserFactory.php
+++ educationalSite/database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'user_role_id' => fake()->numberBetween(1, 4),
         ];
     }
 
@@ -41,4 +42,34 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin() : static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_role_id' => 1,
+        ]);
+    }
+
+    public function teacher() : static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_role_id' => 2,
+        ]);
+    }
+
+    public function currentStudent() : static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_role_id' => 3,
+        ]);
+    }
+
+    public function oldStudent() : static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_role_id' => 4,
+        ]);
+    }
+
+
 }
