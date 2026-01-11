2b523d50c7a19306e34901300eda867d90f869a5
smritidhimal <dsmriti23@tbc.edu.np>
2026-01-12 02:32:32 +0545

    modified UserSeeder

--- educationalSite/database/seeders/UserSeeder.php
+++ educationalSite/database/seeders/UserSeeder.php
@@ -14,58 +14,42 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            
-            'name'=>'Admin User',
-            'email'=>'admin@gmail.com',
-            'password'=>Hash::make('password'),
-            'user_role_id'=>1,
-            'created_at'=> now(),
-            'updated_at'=>now(),
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'user_role_id' => 1,
+            'email_verified_at' => now(),
         ]);
 
-        DB::table('users')->insert([
-            [
-                'name'=>'Teacher 1',
-                'email'=>'teacher1@gmail.com',
-                'password'=>Hash::make('password'),
-                'user_role_id'=>2,
-                'created_at'=> now(),
-                'updated_at'=>now(),
-            ],
-            [
-                'name'=>'Teacher 2',
-                'email'=>'teacher2@gmail.com',
-                'password'=>Hash::make('password'),
-                'user_role_id'=>2,
-                'created_at'=> now(),
-                'updated_at'=>now(),
-            ],
-        ]);
-        $students=[];
-        for($i=1;$i<=5;$i++){
-            $students[]=[
-                'name'=>'Student User',
-                'email'=>'student@gmail.com',
-                'password'=>Hash::make('password'),
-                'user_role_id'=>3,
-                'created_at'=> now(),
-                'updated_at'=>now(),
-            ];
+        for ($i = 1; $i <=2; $i++){
+            User::create([
+                'name' => "Teacher User {$i}",
+                'email' => "teacher{$i}@gmail.com",
+                'password' => Hash::make('password'),
+                'user_role_id' => 2,
+                'email_verified_at' =>now(),
+            ]);
         }
-        DB::table('users')->insert($students);
-         
-        $oldstudents=[];
-        for($i=1;$i<=2;$i++){
-            $oldstudents[]=[
-                'name'=>'Old Student User',
-                'email'=>'oldstudent@gmail.com',
-                'password'=>Hash::make('password'),
-                'user_role_id'=>4,
-                'created_at'=> now(),
-                'updated_at'=>now(),
-            ];
+
+        for ($i = 1; $i <=5; $i++){
+            User::create([
+                'name' => "Student User {$i}",
+                'email' => "Student{$i}@gmail.com",
+                'password' => Hash::make('password'),
+                'user_role_id' => 3,
+                'email_verified_at' =>now(),
+            ]);
+            }
+
+            for ($i = 1; $i <=2; $i++){
+            User::create([
+                'name' => "Old Student User {$i}",
+                'email' => "oldstudent{$i}@gmail.com",
+                'password' => Hash::make('password'),
+                'user_role_id' => 4,
+                'email_verified_at' =>now(),
+            ]);
         }
-        DB::table('users')->insert($oldstudents);
     }
 }
