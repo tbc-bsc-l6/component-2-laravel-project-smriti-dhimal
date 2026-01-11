3f58b9fd64ba342504936a89a3ef74379fbaf368
smritidhimal <dsmriti23@tbc.edu.np>
2026-01-12 02:07:32 +0545

    Add CheckUserRole middleware for route protection

--- educationalSite/bootstrap/app.php
+++ educationalSite/bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->alias([
+            'role'=>\App\Http\Middleware\CheckUserRole::class,
+
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
