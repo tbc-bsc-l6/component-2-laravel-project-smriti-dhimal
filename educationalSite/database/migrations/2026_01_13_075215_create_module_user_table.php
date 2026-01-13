8f9483fd015a6e9cd089ad694983e78cf47a710b
smritidhimal <dsmriti23@tbc.edu.np>
2026-01-13 13:52:15 +0545

    created module user table migration

---
+++ educationalSite/database/migrations/2026_01_13_075215_create_module_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('module_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('module_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('start_date')->nullable();
+            $table->datetime('completed_at')->nullable();
+            $table->enum('result', ['PASS', 'FAIL'])->nullable();
+            $table->timestamps();
+
+            $table->unique(['module_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('module_user');
+    }
+};
